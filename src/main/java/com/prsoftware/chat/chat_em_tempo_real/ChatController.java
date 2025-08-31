5f6601138f4a2ded9fa314fc6cf486c8263101a1
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-08-31 14:09:02 -0300

    Update: Criando os comandos de controller

--- src/main/java/com/prsoftware/chat/chat_em_tempo_real/ChatController.java
+++ src/main/java/com/prsoftware/chat/chat_em_tempo_real/ChatController.java
@@ -1,5 +1,18 @@
 package com.prsoftware.chat.chat_em_tempo_real;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+
+//Criando o Controller da Mensagem
+@Controller
 public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public Message send(Message message){
+        return message;
+    }
 
 }
