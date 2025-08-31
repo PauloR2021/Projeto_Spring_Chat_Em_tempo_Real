133d32c12525f342e64b802136ff3f8ecfc137a1
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-08-31 14:06:37 -0300

    Update: Criando o padrão das Mensagem

--- src/main/java/com/prsoftware/chat/chat_em_tempo_real/Message.java
+++ src/main/java/com/prsoftware/chat/chat_em_tempo_real/Message.java
@@ -1,5 +1,37 @@
 package com.prsoftware.chat.chat_em_tempo_real;
 
 public class Message {
+
+    private String sender;
+    private String content;
+
+    public Message(){}
+    
+    
+    public Message(String sender, String content){
+        this.sender = sender;
+        this.content = content;
+    }
+
+
+    public String getSender() {
+        return sender;
+    }
+
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     
 }
